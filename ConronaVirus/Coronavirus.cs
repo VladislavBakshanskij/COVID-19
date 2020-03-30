693e2b3451122a58f308275a8442b341f74892a1
Koro <a89649202326@gmail.com>
2020-03-30 21:02:42 +0300

    Initial commit

---
+++ ConronaVirus/Coronavirus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CoronaVirus {
+    public class Coronavirus {
+        public int Cases { get; set; }
+        public int Death { get; set; }
+        public int Recovered { get; set; }
+        public DateTime Date { get; set; }
+
+        public Coronavirus() { 
+        }
+
+        public Coronavirus(int cases, int death, int recovered) {
+            this.Cases = cases;
+            this.Death = death;
+            this.Recovered = recovered;
+            Date = DateTime.Now;
+        }
+    }
+}
